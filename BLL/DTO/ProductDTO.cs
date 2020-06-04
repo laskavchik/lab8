18d914d751fd6557a982cac45118d3dbc258990f
laskavchik <64552940+laskavchik@users.noreply.github.com>
2020-06-04 22:02:21 +0300

    Bll tets

---
+++ BLL/DTO/ProductDTO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using DAL.Entities;
+
+namespace BLL.DTO
+{
+    public class ProductDto
+    {
+        public int Id { get; set; }
+        public decimal Price { get; set; }
+
+        public static implicit operator ProductDto(Product dal) => new ProductDto
+        {
+            Id = dal.Id,
+            Price = dal.Price
+        };
+    }
+}
